a2fa57b082ce743ffae923a0154cdc981f709a53
Baris Tacirlioglu <baristacirlioglu@gmail.com>
2025-07-17 15:41:57 +0300

    first commit

---
+++ currency_app/README.md
@@ -0,0 +1,26 @@
+# Currency Exchange App 💱
+
+This is a simple currency exchange application built with **React** using a free and public exchange rate API. It allows users to select two currencies and see the real-time conversion rate between them.
+
+> ⚛️ This is my first project using React!
+
+## 🚀 Features
+
+- Live currency exchange rates
+- Select base and target currencies
+- Clean and minimal interface
+- Built using a free and open API
+
+## 🛠️ Tech Stack
+
+- React
+- Axios
+- FreeCurrencyAPI
+
+## 📦 Installation and Starting
+
+Clone the repository:
+git clone https://github.com/baristacirlioglu/currency-exchange-app.git
+cd currency_app
+npm run dev
+open http://localhost:5173
---
+++ currency_app/src/components/Currency.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import '../css/currency.css'
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import axios, { Axios } from 'axios'
+let API_KEY = import.meta.env.VITE_API_KEY
+
+let BASE_URL = "https://api.freecurrencyapi.com/v1/latest";
+
+
+function Currency() {
+    const [amount , setAmount] = useState(0);
+    const [fromCurrency , setFromCurrency] = useState('USD');
+    const [toCurrency, setToCurrency] = useState('TRY');
+    const [result, setResult] = useState(0);
+  
+    const exchange = async () => {
+        if (amount<0) {
+            alert("0'dan kucuk girilemez!");
+            return;
+        }
+        const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`);
+        setResult(((response.data.data[toCurrency]) * amount).toFixed(2));
+
+    }
+  
+  
+    return (
+    <div className='currency-div'>
+        <div style={{marginTop:'-30px'}}>
+            <h3>DÖVİZ KURU UYGULAMASI</h3>
+        </div>
+        
+        <div>
+            <input
+            value = {amount>0 ? amount : 0}
+            onChange={(e) => setAmount(e.target.value)}
+            type="number" className='amount' />
+        <select onChange={(e) => setFromCurrency(e.target.value)} className='from-currency-option'>
+            <option>USD</option>
+            <option>EUR</option>
+            <option>TRY</option>
+        </select>
+
+        <FaRegArrowAltCircleRight style={{fontSize: '30px'}} />
+
+        <select onChange={(e) => setToCurrency(e.target.value)} className='to-currency-option'>
+            <option>TRY</option>
+            <option>USD</option>
+            <option>EUR</option>
+        </select>
+
+        <input
+        value = {result}
+        onChange={(e) => setResult(e.target.value)}
+        type="number" className='result' />
+        </div>
+
+        <div>
+            <button 
+            onClick={exchange}
+            style={{marginTop: '20px', width: '100px', height: '30px', backgroundColor: 'pink', borderRadius:'5px', cursor:'pointer'}}>ÇEVİR</button>
+        </div>
+        
+        
+    </div>
+  )
+}
+
+export default Currency;
